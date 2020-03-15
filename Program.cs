bfe22068dd565322d30429d08a00ad0618c78f27
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-15 16:55:15 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ALDS1._1.A.Run();
+        }
+    }
+}
